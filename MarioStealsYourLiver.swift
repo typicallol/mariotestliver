d366439e3db4201cd2c63edd4d70abad796dbba1
typicallol <120810615+typicallol@users.noreply.github.com>
2025-01-01 15:52:06 -0800

    Update MarioStealsYourLiver.swift
    Changes the action of the cancel button, instead of displaying the same message, the cancel button is removed after pressing it once

--- MarioStealsYourLiver.swift
+++ MarioStealsYourLiver.swift
@@ -5,6 +5,7 @@ import PlaygroundSupport
 class ViewController: UIViewController {
     
     var audioPlayer: AVAudioPlayer!
+    var showCancelButton: Bool = true // Track whether to show the Cancel button
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,43 +22,46 @@ class ViewController: UIViewController {
     
     func displayAlert() {
         // Declare Alert message
-        let dialogMessage = UIAlertController(title: "3days", message: "3 days until mario steals your liver", preferredStyle: .alert)
+        let dialogMessage = UIAlertController(
+            title: "3days", 
+            message: "3 days until mario steals your liver", 
+            preferredStyle: .alert
+        )
         
         // Create OK button with action handler
         let ok = UIAlertAction(title: "ok", style: .default) { (action) -> Void in
             print("Ok button tapped")
         }
-        
-        // Create Cancel button with action handler
-        let cancel = UIAlertAction(title: "cancel", style: .cancel) { (action) -> Void in
-            print("Cancel button tapped")
-            
-            // Play another sound
-            if let sound = Bundle.main.path(forResource: "itsnotaoption", ofType: "wav") {
-                self.audioPlayer = try? AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound))
-                self.audioPlayer.prepareToPlay()
-                self.audioPlayer.play()
-            }
-            
-            // Re-display the alert after a delay
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                self.displayAlert()
-            }
-        }
-        
-        // Add OK and Cancel button to dialog message
         dialogMessage.addAction(ok)
-        dialogMessage.addAction(cancel)
+        
+        // Conditionally add the Cancel button
+        if showCancelButton {
+            let cancel = UIAlertAction(title: "cancel", style: .cancel) { (action) -> Void in
+                print("Cancel button tapped")
+                
+                // Play another sound
+                if let sound = Bundle.main.path(forResource: "itsnotaoption", ofType: "wav") {
+                    self.audioPlayer = try? AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound))
+                    self.audioPlayer.prepareToPlay()
+                    self.audioPlayer.play()
+                }
+                
+                // Disable the Cancel button for subsequent alerts
+                self.showCancelButton = false
+                
+                // Re-display the alert after a delay
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    self.displayAlert()
+                }
+            }
+            dialogMessage.addAction(cancel)
+        }
         
         // Present dialog message to user
         self.present(dialogMessage, animated: true, completion: nil)
     }
-    
-    func deleteRecord() {
-        print("Delete record function called")
-    }
 }
-
+    
 // Present the view controller in the playground
 let viewController = ViewController()
 PlaygroundPage.current.liveView = viewController
